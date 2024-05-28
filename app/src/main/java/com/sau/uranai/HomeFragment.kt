1eac3b96cd526199715dc430c6ede175d5623f54
Mert Cihan Bayır <mert.bayir@ogr.sakarya.edu.tr>
2024-05-28 17:33:12 +0300

    Added cardViews and font

--- app/src/main/java/com/sau/uranai/HomeFragment.kt
+++ app/src/main/java/com/sau/uranai/HomeFragment.kt
@@ -1,18 +1,47 @@
 package com.sau.uranai
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.sau.uranai.FortuneActivity
+import com.sau.uranai.R
 
-class HomeFragment:Fragment() {
+class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        setupCardViewListeners(view)
+        return view
     }
 
+    private fun setupCardViewListeners(view: View) {
+        // Kahve Falı CardView
+        view.findViewById<CardView>(R.id.kahve_cardView).setOnClickListener {
+            startActivity(Intent(activity, FortuneActivity::class.java))
+        }
+
+        // El Falı CardView
+        view.findViewById<CardView>(R.id.elfali_cardView).setOnClickListener {
+            showToastMessage()
+        }
+
+        // Tarot Falı CardView
+        view.findViewById<CardView>(R.id.tarot_cardView).setOnClickListener {
+            showToastMessage()
+        }
+
+    }
+    private fun showToastMessage() {
+        Toast.makeText(requireContext(), "Coming Soon...", Toast.LENGTH_SHORT).show()
+    }
 }
--- app/src/main/java/com/sau/uranai/PastFortunesFragment.kt
+++ app/src/main/java/com/sau/uranai/PastFortunesFragment.kt
@@ -1,5 +1,6 @@
 package com.sau.uranai
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 
 
@@ -18,7 +20,16 @@ class PastFortunesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pastfortunes, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_pastfortunes, container, false)
 
+        setupCardViewListeners(view)
+        return view
+    }
+    private fun setupCardViewListeners(view: View) {
+
+        view.findViewById<CardView>(R.id.PastFortunes_cardView).setOnClickListener {
+            Toast.makeText(requireContext(), "Coming Soon...", Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
